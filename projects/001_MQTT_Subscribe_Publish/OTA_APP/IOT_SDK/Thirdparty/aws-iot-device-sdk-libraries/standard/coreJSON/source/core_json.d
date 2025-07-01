571419ebd5e8f1d56fa3edd4889e2d4da25a1d69
Kiran Nayak <kiran@bharatisoft.in>
2025-07-01 14:35:58 +0530

    Add project 001_MQTT_Subscribe_Publish

---
+++ projects/001_MQTT_Subscribe_Publish/OTA_APP/IOT_SDK/Thirdparty/aws-iot-device-sdk-libraries/standard/coreJSON/source/core_json.d
@@ -0,0 +1,4 @@
+IOT_SDK/Thirdparty/aws-iot-device-sdk-libraries/standard/coreJSON/source/core_json.o: \
+ ../IOT_SDK/Thirdparty/aws-iot-device-sdk-libraries/standard/coreJSON/source/core_json.c \
+ C:/Users/shrey/STM32CubeIDE/fastbit-stm32WB-IOT-board/Projects-and-examples/001_MQTT_Subscribe_Publish/IOT_SDK/Thirdparty/aws-iot-device-sdk-libraries/standard/coreJSON/source/include/core_json.h
+C:/Users/shrey/STM32CubeIDE/fastbit-stm32WB-IOT-board/Projects-and-examples/001_MQTT_Subscribe_Publish/IOT_SDK/Thirdparty/aws-iot-device-sdk-libraries/standard/coreJSON/source/include/core_json.h:
