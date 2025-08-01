9dde02117f2a6a821e5a921ebcee852fcc65caeb
Shreyas Acharya <shreyasacharya49@gmail.com>
2025-08-01 17:45:17 +0530

    Add project 002_OTA_Firmware_Update

---
+++ projects/002_OTA_Firmware_Update/Core/Inc/custom_job_doc.h
@@ -0,0 +1,26 @@
+/*
+ * custom_job_doc.h
+ *
+ *  Created on: Apr 15, 2025
+ *      Author: Shreyas Acharya, BHARATI SOFTWARE
+ */
+
+#ifndef INC_CUSTOM_JOB_DOC_H_
+#define INC_CUSTOM_JOB_DOC_H_
+
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+typedef struct
+{
+  const char *image_ref;         // "streamID"
+  size_t image_ref_len;
+
+  uint32_t file_id;
+
+  uint32_t file_size;            // files[0].fileSize
+} custom_job_doc_fields_t;
+
+#endif /* INC_CUSTOM_JOB_DOC_H_ */
